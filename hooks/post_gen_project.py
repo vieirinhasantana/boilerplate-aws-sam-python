8d4d55310ae5464abf9f67cd83cdbb5a6098abbf
Daniel V Santana <vieirinhasantana@gmail.com>
2020-10-18 10:59:34 -0300

    Create new rule validation init project

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,7 +1,5 @@
 from __future__ import print_function
 
-import os
-
 TERMINATOR = "\x1b[0m"
 INFO = "\x1b[1;33m [INFO]: "
 SUCCESS = "\x1b[1;32m [SUCCESS]: "
@@ -12,12 +10,8 @@ def main():
 
     project_name = '{{ cookiecutter.project_name }}'
 
-    print(SUCCESS +
-          "Project initialized successfully! You can now jump to {} folder".
-          format(project_name) + TERMINATOR)
-    print(INFO +
-          "{}/README.md contains instructions on how to proceed.".
-          format(project_name) + TERMINATOR)
+    print(f"{SUCCESS} Project initialized successfully! You can now jump to {project_name} folder. {TERMINATOR}")
+    print(f"{INFO} {project_name}/README.md contains instructions on how to proceed. {TERMINATOR}")
 
 
 if __name__ == '__main__':
--- {{ cookiecutter.project_name }}/tests/conftest.py
+++ {{ cookiecutter.project_name }}/tests/conftest.py
@@ -1,22 +1,35 @@
+import os
 import json
 import pytest
+
+from moto import mock_s3
 from uuid import uuid4
 
 
 class MockContext(object):
-
     def __init__(self, function_name):
         self.function_name = function_name
         self.function_version = "v$LATEST"
-        self.memory_limit_in_mb = 128
-        self.invoked_function_arn = f"arn:aws:lambda:us-east-1:ACCOUNT:function:{self.function_name}"
+        self.memory_limit_in_mb = '{{ cookiecutter.memory_limit_lambda }}'
+        self.invoked_function_arn = f"arn:aws:lambda:{os.getenv('AWS_REGION_NAME')}:ACCOUNT:function:{self.function_name}"
         self.aws_request_id = str(uuid4)
 
+@pytest.fixture(scope='function')
+def aws_credentials():
+    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+    os.environ['AWS_SESSION_TOKEN'] = 'testing'
+    os.environ['AWS_REGION_NAME'] = 'us-east-1'
+
+@pytest.fixture(scope='function')
+def s3(aws_credentials):
+    with mock_s3():
+        yield boto3.client('s3', region_name=os.getenv('AWS_REGION_NAME'))
 
 @pytest.fixture
 def lambda_context():
     return MockContext("dummy_function")
-
 
 @pytest.fixture()
 def apigw_event():
